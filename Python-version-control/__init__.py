eac9654249f3b13b33a041f4740e96a1c4f1f4b3
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-05-10 09:51:32 +1000

    started making the code infrastructure

---
+++ Python-version-control/__init__.py
@@ -0,0 +1,16 @@
+"""The Python Version control software."""
+
+
+class PVC(object):
+	"""The main class for PVC."""
+	def __init__(dir):
+		"""The initialiser for The main PVC object"""
+		self.dir = dir
+
+	def force(command):
+		"""Base command for forcing git commands."""
+		pass
+
+	def messege(commnad):
+		"""Base command for adding messeges to git commands."""
+		pass
