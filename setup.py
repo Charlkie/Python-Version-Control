71cfb8726b886a1ad857c6a4a7ef1f02c26040de
“Charl <Charl.kruger@me.com>
2018-05-09 18:02:15 +1000

    hello!

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 setup(
-    name="PVC",
-    packages =["PVC"],
+    name="pvc_package",
+    packages =["pvc_package"],
     version = "0.1",
     description="A python version control system",
     author="Charl Kruger, Stephan Kashkarov",
-    author_email="charl.kruger@me.com"
-
+    author_email="charl.kruger@me.com, Kashkarovwork@gmail.com",
+    url="https://github.com/Charlkie/Python-Version-Control",
+    download_url="https://github.com/Charlkie/Python-Version-Control/archive/0.1.tar.gz",
+    keyword=["version control system", "version control"],
+    classifiers=[]
 )
