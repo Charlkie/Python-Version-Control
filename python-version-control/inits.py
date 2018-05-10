9cde8f4abe51324d82ff40e207d79b85032cfecd
“Charl <Charl.kruger@me.com>
2018-05-10 10:49:15 +1000

    More setup

--- python-version-control/inits.py
+++ python-version-control/inits.py
@@ -16,16 +16,7 @@ class Init():
         #for folder in self.folders:
             #os.makedirs(os.path.join('.pvc',folder))
 
-    def test(self):
-        hash = hash.Hash_Obj()
-        #keep these
-        hash.hash_obj_dir(".pyvcs/objects", folders)
-        added_files=['test.txt']
-        folders= [(hash.hash_file(file)[0:2],hash.hash_file(file)[2:-1]) for file in added_files]
-        print(folders[0][0])
-        hash.create_blob(folders, added_files)
-        #keep these
-        #print(input("file to decode: "))
+
 
 init = Init()
 init.init()
