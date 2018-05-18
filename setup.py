8a2673d6d807dc59f4f670d538ec27022aa27ef3
Stephan Kashkarov <kashkarovWork@gmail.com>
2018-05-18 12:37:59 +1000

    did pypi stuff

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name="Python-version-control",
     packages=["Python-version-control"],
-    version="0.1",
+    version="0.2",
     description="A python version control system",
     author="Charl Kruger, Stephan Kashkarov",
     author_email="charl.kruger@me.com, Kashkarovwork@gmail.com",
     url="https://github.com/Charlkie/Python-Version-Control",
-    download_url="https://github.com/Charlkie/Python-Version-Control/archive/0.1.tar.gz",
+    download_url="https://github.com/Charlkie/Python-Version-Control/archive/0.2.tar.gz",
     keyword=["version control system", "version control"],
     classifiers=[]
 )
